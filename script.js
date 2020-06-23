3b3e954e2caf43d1710d3124d40e332f546760b5
Kha Truong <khatruong2009@gmail.com>
2020-06-23 13:18:06 -0400

    basic calculator layout

---
+++ script.js
@@ -0,0 +1,21 @@
+let add = function(num1, num2) {
+ return num1 + num2;
+}
+
+let subtract = function(num1, num2) {
+ return num1 - num2;
+}
+
+let multiply = function(num1, num2) {
+ return num1 * num2;
+}
+
+let divide = function(num1, num2) {
+ return num1 / num2;
+}
+
+function operate(num1, num2, math) {
+ return math(num1, num2);
+}
+
+console.log(operate(8, 2, divide));
