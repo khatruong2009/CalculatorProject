48d9d03415bb2960c89ce39a9a91d4b8a14ce857
Kha Truong <khatruong2009@gmail.com>
2020-06-25 11:56:55 -0400

    beginning to add javascript

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+let display = document.querySelector(".displayValue");
+let keys = document.querySelector(".keys");
+let displayValue = 0;
+display.innerHTML = displayValue;
+let numbers = document.querySelector(".number");
+let clear = document.querySelector("#clear");
 
+clear.addEventListener("click", () => {
+ displayValue = 0;
+})
 
+keys.addEventListener("click", e => {
+ if (e.target.matches("button")) {
 
+ }
+}
+)
+
+//operate function
+function operate(num1, num2, math) {
+ return math(num1, num2);
+}
 
 //math functions
 function add(num1, num2) {
